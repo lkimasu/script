f4881e1d45d111ba25293ed80df28a23b4fe13cd
lkimasu <lwlstjdl@naver.com>
2024-03-05 13:31:03 +0900

    update get to urlopen

--- apache_info/main.py
+++ apache_info/main.py
@@ -5,7 +5,6 @@ from urllib import request
 import config
 
 def send_email(subject, body):
-
     msg = MIMEMultipart()
     msg['From'] = config.EMAIL_ADDRESS
     msg['To'] = config.RECIPIENT_EMAIL
@@ -21,15 +20,16 @@ def send_email(subject, body):
 
 def check_web_server(url):    
     try:
-        response = request.get(url)
-        if response.status_code == 200:
+        response = request.urlopen(url)
+        if response.status == 200:
             print("웹 서버가 정상적으로 동작 중입니다.")
         else:
-            print(f"서버 응답 상태 코드: {response.status_code}")
+            print(f"서버 응답 상태 코드: {response.status}")
             print("웹 서버에 문제가 있을 수 있습니다.")
             send_email("웹 서버 오류 발생", "웹 서버에 문제가 있습니다. 조치가 필요합니다.")
     except Exception as e:
         print(f"에러 발생: {e}")
+        send_email("웹 서버 오류 발생", f"웹 서버에 오류가 발생했습니다. 에러 메시지: {e}")
 
 def main():
     apache_url = "http://your-apache-server/"
